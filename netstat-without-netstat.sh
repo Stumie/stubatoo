932b866c2885f9e592a3b132388be4383936189e
Stumie <Stumie123@googlemail.com>
2023-01-29 17:12:07 +0100

    add netstat-without-netstat.sh based on qistoph/awk_netstat.sh

---
+++ netstat-without-netstat.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+### Variable declarations ###
+
+THISSCRIPTPATH=$(readlink -f $0)
+THISDIRPATH=$(dirname $THISSCRIPTPATH)
+SUBSCRIPT=$THISDIRPATH/.scripts
+SUBTREES=$THISDIRPATH/.subtrees
+
+### Function declarations ###
+
+get-connections () {
+  source $SUBTREES/awk_netstat/awk_netstat.sh
+}
+
+### Procedures ###
+
+$SUBSCRIPT/check-for-software-existence.sh awk column
+
+get-connections | column -t
