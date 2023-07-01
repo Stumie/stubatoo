9d921ac7fecdf49aca03efe4e5ec34f4fc84e5bf
Stumie <Stumie123@googlemail.com>
2023-07-01 16:00:44 +0200

    Added .wine-prefix-orders/_template-order.sh as a wine-prefix-order template using the new abstraction layer

---
+++ .wine-prefix-orders/_template-order.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Usage: $WINEPREFIXORDERS/$REQUESTEDWINEPREFIXORDER.sh $WINEPREFIXFOLDER
+
+### Constant declarations ###
+
+WINEARCH="win64"
+
+### Imports and variable declarations ###
+
+THISSCRIPTPATH=$(readlink -f $0)
+THISDIRPATH=$(dirname $THISSCRIPTPATH)
+SUBSCRIPT=$THISDIRPATH/../.scripts
+
+source $SUBSCRIPT/wine-install-abstraction-layer.sh
+
+### Procedures ###
+
+# Outcomment the lines below to start using the template 
+#wine-prepare
+#wine-set-winver win7
+
+$SUBSCRIPT/highlighted-output.sh \
+  "The only is a template. Use it for copy-and-paste."
