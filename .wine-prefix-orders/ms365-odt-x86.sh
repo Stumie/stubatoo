b2bb8a96fa092996a27b4894c9bd6c01bc251266
Stumie <Stumie123@googlemail.com>
2023-08-12 18:51:49 +0200

    .wine-prefix-orders/ms365-odt-x86.sh: moved this order to use the new abstraction layer

--- .wine-prefix-orders/ms365-odt-x86.sh
+++ .wine-prefix-orders/ms365-odt-x86.sh
@@ -19,54 +19,48 @@ THISSCRIPTPATH=$(readlink -f $0)
 THISDIRPATH=$(dirname $THISSCRIPTPATH)
 SUBSCRIPT=$THISDIRPATH/../.scripts
 
-WINEPREFIXNAME=$(basename -s .sh $0)
-WINEPREFIXFOLDER=$1
-FULLWINEPREFIXPATH=$WINEPREFIXFOLDER/$WINEPREFIXNAME
+source $SUBSCRIPT/wine-install-abstraction-layer.sh
 
 DOWNLOADFOLDER=$WINEPREFIXFOLDER/tmp-downloads/$WINEPREFIXNAME
-
 ODTEXEDOWNLOADLINK="$(curl -s "$ODTDOWNLOADPAGE" | grep -Po 'href="https://[^"]+\.(exe)"' | grep -Po 'https://[^"]+' | tail -1)"
 SETUPFILENAME=$(basename $ODTEXEDOWNLOADLINK)
 SETUPFILEPATH=$DOWNLOADFOLDER/$SETUPFILENAME
-
 WEBVIEWSETUPPATH=$DOWNLOADFOLDER/$WEBVIEWSETUPFILENAME
 
 ### Procedures ###
 
-source $SUBSCRIPT/wine-install-winetricks-verbs.sh
+wine-prepare
 
 # Download ODT
-$SUBSCRIPT/wine-prefix-download-software.sh $WINEPREFIXFOLDER $WINEPREFIXNAME $ODTEXEDOWNLOADLINK || { printf '%s\n' "ERROR! Could not download file!" >&2 && exit 1; }
+download $ODTEXEDOWNLOADLINK
 
 # Download MS Edge WebView
-$SUBSCRIPT/wine-prefix-download-software-followlink.sh $WINEPREFIXFOLDER $WINEPREFIXNAME $WEBVIEWDOWNLOADLINK $WEBVIEWSETUPFILENAME  || { printf '%s\n' "ERROR! Could not download file!" >&2 && exit 1; }
+download-followlink $WEBVIEWDOWNLOADLINK $WEBVIEWSETUPFILENAME
 
 # Download elscore.dll
-$SUBSCRIPT/wine-prefix-download-software.sh $WINEPREFIXFOLDER $WINEPREFIXNAME $ELSCOREATWINEDLLDOTCOM || { printf '%s\n' "ERROR! Could not download file!" >&2 && exit 1; }
+download $ELSCOREATWINEDLLDOTCOM
 
-# Prepare first run of Wine prefix
-$SUBSCRIPT/wine-prefix-prepare-first-run.sh $WINEARCH $WINEPREFIXFOLDER $WINEPREFIXNAME || { printf '%s\n' "ERROR! Could not prepare wine prefix!" >&2 && exit 1; }
+# Set Windows Version to Windows 7
+wine-set-winver win7
 
-# Install the relevant set of wintricks
-install-winetricks-verbs win7 riched20 msxml6 corefonts tahoma pptfonts
+# Install necessary prerequisites 
+wine-install-prerequisites riched20 msxml6 corefonts tahoma pptfonts
 
 # Add Wine registry keys for workarounds
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\Direct2D" /v "max_version_factory" /t REG_DWORD /d "0" /f
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\Direct3D" /v "MaxVersionGL" /t REG_DWORD /d "196610" /f  
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\DllOverrides" /v "sppc" /t REG_SZ /d "" /f
+wine-reg-add "HKCU\Software\Wine\Direct2D" "max_version_factory" "REG_DWORD" "0"
+wine-reg-add "HKCU\Software\Wine\Direct3D" "MaxVersionGL" "REG_DWORD" "196610"
+wine-reg-add "HKCU\Software\Wine\DllOverrides" "sppc" "REG_SZ" ""
 
 # Make sure Wine prefix is set to Windows 7
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH winecfg -v win7
+wine-set-winver win7
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 # Install Microsoft Edge WebView2 Runtime
 $SUBSCRIPT/highlighted-output.sh \
   "The script will now install Microsoft Edge WebView2 Runtime in the background." \
   "Install Mono and Gecko, if any windows ask for it."
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine $WEBVIEWSETUPPATH /silent /install
+wine-execute $WEBVIEWSETUPPATH /silent /install
 
 # Prepare configuration for Office Deployment Tool
 # Manual: https://learn.microsoft.com/en-us/deployoffice/office-deployment-tool-configuration-options
@@ -85,18 +79,16 @@ cat << EOF > $FULLWINEPREFIXPATH/drive_c/ODT/installOfficeProPlus32.xml
 EOF
 
 # Create Setup with Office Deployment Tool
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine $SETUPFILEPATH /quiet /passive /norestart /extract:$FULLWINEPREFIXPATH/drive_c/ODT/
+wine-execute $SETUPFILEPATH /quiet /passive /norestart /extract:$FULLWINEPREFIXPATH/drive_c/ODT/
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 $SUBSCRIPT/highlighted-output.sh \
   "The script will now start the Microsoft Office 365 installer." \
   "Follow the installer's instructions, if necessary." \
   "Install Mono and Gecko, if any windows ask for it."
 
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine $FULLWINEPREFIXPATH/drive_c/ODT/setup.exe /configure "C:\ODT\installOfficeProPlus32.xml"
+wine-execute $FULLWINEPREFIXPATH/drive_c/ODT/setup.exe /configure "C:\ODT\installOfficeProPlus32.xml"
 
 # Workaround: Symlink creation seems to be broken during installation, which is the reason, why DLLs have to be copied manually.
 cp -fv $FULLWINEPREFIXPATH/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/ClickToRun/*.dll $FULLWINEPREFIXPATH/drive_c/Program\ Files/Microsoft\ Office/root/Office16/
@@ -110,9 +102,7 @@ mv -fv $DOWNLOADFOLDER/ELSCore.dll $FULLWINEPREFIXPATH/drive_c/Program\ Files/Mi
 # Remove ODT folder to save disk space
 rm -rf $FULLWINEPREFIXPATH/drive_c/ODT
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 $SUBSCRIPT/highlighted-output.sh \
   "Installation finished." \
