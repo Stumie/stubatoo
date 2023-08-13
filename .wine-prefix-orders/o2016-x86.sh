9fbf091670e4a5e20aedc78b5b4bf0f15ca8aeb0
Stumie <Stumie123@googlemail.com>
2023-08-13 20:42:27 +0200

    .wine-prefix-orders/o2016-x86.sh: moved this order to use the new abstraction layer

--- .wine-prefix-orders/o2016-x86.sh
+++ .wine-prefix-orders/o2016-x86.sh
@@ -15,17 +15,13 @@ THISSCRIPTPATH=$(readlink -f $0)
 THISDIRPATH=$(dirname $THISSCRIPTPATH)
 SUBSCRIPT=$THISDIRPATH/../.scripts
 
-WINEPREFIXNAME=$(basename -s .sh $0)
-WINEPREFIXFOLDER=$1
-FULLWINEPREFIXPATH=$WINEPREFIXFOLDER/$WINEPREFIXNAME
+source $SUBSCRIPT/wine-install-abstraction-layer.sh
 
 DOWNLOADFOLDER=$WINEPREFIXFOLDER/tmp-downloads/$WINEPREFIXNAME
-
 WEBVIEWSETUPPATH=$DOWNLOADFOLDER/$WEBVIEWSETUPFILENAME
 
 ### Procedures ###
 
-source $SUBSCRIPT/wine-install-winetricks-verbs.sh
 source $SUBSCRIPT/obtain-filepath.sh
 
 $SUBSCRIPT/highlighted-output.sh \
@@ -38,47 +34,43 @@ if [ $O2016SETUPFILEPATH == "" ] || ! [[ "$O2016SETUPFILEPATH" =~ .*".exe" ]]; t
   exit 1
 fi
 
+wine-prepare
+
 # Download MS Edge WebView
-$SUBSCRIPT/wine-prefix-download-software-followlink.sh $WINEPREFIXFOLDER $WINEPREFIXNAME $WEBVIEWDOWNLOADLINK $WEBVIEWSETUPFILENAME  || { printf '%s\n' "ERROR! Could not download file!" >&2 && exit 1; }
+download-followlink $WEBVIEWDOWNLOADLINK $WEBVIEWSETUPFILENAME
 
-# Prepare first run of Wine prefix
-$SUBSCRIPT/wine-prefix-prepare-first-run.sh $WINEARCH $WINEPREFIXFOLDER $WINEPREFIXNAME || { printf '%s\n' "ERROR! Could not prepare wine prefix!" >&2 && exit 1; }
+# Set Windows Version to Windows 7
+wine-set-winver win7
 
-# Install the relevant set of wintricks
-install-winetricks-verbs win7 riched20 msxml6 corefonts tahoma pptfonts
+# Install necessary prerequisites 
+wine-install-prerequisites riched20 msxml6 corefonts tahoma pptfonts
 
 # Add Wine registry keys for workarounds
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\Direct2D" /v "max_version_factory" /t REG_DWORD /d "0" /f
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\Direct3D" /v "MaxVersionGL" /t REG_DWORD /d "196610" /f  
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine reg add "HKCU\Software\Wine\DllOverrides" /v "sppc" /t REG_SZ /d "" /f
+wine-reg-add "HKCU\Software\Wine\Direct2D" "max_version_factory" "REG_DWORD" "0"
+wine-reg-add "HKCU\Software\Wine\Direct3D" "MaxVersionGL" "REG_DWORD" "196610"
+wine-reg-add "HKCU\Software\Wine\DllOverrides" "sppc" "REG_SZ" ""
 
 # Make sure Wine prefix is set to Windows 7
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH winecfg -v win7
+wine-set-winver win7
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 # Install Microsoft Edge WebView2 Runtime
 $SUBSCRIPT/highlighted-output.sh \
   "The script will now install Microsoft Edge WebView2 Runtime in the background." \
   "Install Mono and Gecko, if any windows ask for it."
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine $WEBVIEWSETUPPATH /silent /install
+wine-execute $WEBVIEWSETUPPATH /silent /install
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 $SUBSCRIPT/highlighted-output.sh \
   "The script will now start the Microsoft Office 2016 installer." \
   "Follow the installer's instructions, if necessary." \
   "Install Mono and Gecko, if any windows ask for it."
 
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wine $O2016SETUPFILEPATH
+wine-execute $O2016SETUPFILEPATH
 
-# Update and reboot wine prefix
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -u
-WINEPREFIX=$FULLWINEPREFIXPATH WINEARCH=$WINEARCH wineboot -r
+wine-reboot
 
 $SUBSCRIPT/highlighted-output.sh \
   "Installation finished." \
