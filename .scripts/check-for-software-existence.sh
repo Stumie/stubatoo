b6063d32eb5f7ce7136d29b11d28d846f0686529
Stumie <Stumie123@googlemail.com>
2024-10-03 13:24:33 +0200

    .scripts/check-for-software-existence.sh: removed unnecessary indentation

--- .scripts/check-for-software-existence.sh
+++ .scripts/check-for-software-existence.sh
@@ -2,13 +2,13 @@
 
 ### Procedures ###
 
-  for i in $@
-  do
-    if ! command -v $i &> /dev/null; then
-      printf '%s\n' "ERROR! The software package '$i' is necessary, but could not be found, exiting."
-      if command -v apt-get &> /dev/null; then
-        printf '%s\n' "You might want to try something like 'sudo apt-get update && sudo apt-get install $i' to install it."
-      fi
-      exit 1
+for i in $@
+do
+  if ! command -v $i &> /dev/null; then
+    printf '%s\n' "ERROR! The software package '$i' is necessary, but could not be found, exiting."
+    if command -v apt-get &> /dev/null; then
+      printf '%s\n' "You might want to try something like 'sudo apt-get update && sudo apt-get install $i' to install it."
     fi
-  done
+    exit 1
+  fi
+done
