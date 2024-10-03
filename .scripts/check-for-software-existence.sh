dd38a445e7f925d81424aeabea9a6a50e338cb6a
Stumie <Stumie123@googlemail.com>
2024-10-03 13:21:31 +0200

    .scripts/check-for-software-existence.sh: improved error message and added installation hint for systems with apt-get

--- .scripts/check-for-software-existence.sh
+++ .scripts/check-for-software-existence.sh
@@ -5,7 +5,10 @@
   for i in $@
   do
     if ! command -v $i &> /dev/null; then
-      printf '%s\n' "ERROR! $i is necessary, but could not be found, exiting."
+      printf '%s\n' "ERROR! The software package '$i' is necessary, but could not be found, exiting."
+      if command -v apt-get &> /dev/null; then
+        printf '%s\n' "You might want to try something like 'sudo apt-get update && sudo apt-get install $i' to install it."
+      fi
       exit 1
     fi
   done
