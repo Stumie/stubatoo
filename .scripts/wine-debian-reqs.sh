bb33a3e96cb42a59ae117a4a3a54b7b12f226dd9
Stumie <Stumie123@googlemail.com>
2024-10-04 22:53:22 +0200

    .scripts/wine-debian-reqs.sh: untested streamlining

--- .scripts/wine-debian-reqs.sh
+++ .scripts/wine-debian-reqs.sh
@@ -24,30 +24,30 @@ WINEBRANCHNAME=$1
 
 ### Procedures - Part 2 ###
 
-if ! [ "$WINEBRANCHNAME" != "$WINESTABLEBRANCH" ] && [ "$WINEBRANCHNAME" != "$WINESTAGINGBRANCH" ] && [ "$WINEBRANCHNAME" != "$WINEDEVELBRANCH" ]; then
-
-  rm /etc/apt/sources.list.d/winehq.sources /etc/apt/sources.list.d/winehq-*.sources
-  wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-  wget -nc -O /etc/apt/sources.list.d/winehq.sources https://dl.winehq.org/wine-builds/$DISTRIRELEASE/dists/$DISTRIVERSION/winehq-$DISTRIVERSION.sources
-  mkdir -pm755 /etc/apt/keyrings
-  cp -fv /usr/share/keyrings/winehq-archive.key /etc/apt/keyrings/winehq-archive.key
-
-  apt-get update -y
-  apt-get upgrade -y
-  apt-get install --install-recommends winehq-$WINEBRANCHNAME -y
-  apt-get install winetricks cabextract p11-kit p11-kit-modules winbind samba smbclient -y
-
-fi
-
-if [[ "$WINEBRANCHNAME" = "bottles" ]]; then
-
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get -y install flatpak winetricks coreutils grep fzf jq
-  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  sudo flatpak update -y
-  sudo flatpak install flathub com.usebottles.bottles
-
-fi
+case "$WINEBRANCHNAME" in
+  "bottles")
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get -y install flatpak winetricks jq
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    sudo flatpak update -y
+    sudo flatpak install flathub com.usebottles.bottles
+    ;;
+  "$WINESTABLEBRANCH" | "$WINESTAGINGBRANCH" | "$WINEDEVELBRANCH")
+    rm /etc/apt/sources.list.d/winehq.sources /etc/apt/sources.list.d/winehq-*.sources
+    wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+    wget -nc -O /etc/apt/sources.list.d/winehq.sources https://dl.winehq.org/wine-builds/$DISTRIRELEASE/dists/$DISTRIVERSION/winehq-$DISTRIVERSION.sources
+    mkdir -pm755 /etc/apt/keyrings
+    cp -fv /usr/share/keyrings/winehq-archive.key /etc/apt/keyrings/winehq-archive.key
+    apt-get update -y
+    apt-get upgrade -y
+    apt-get install --install-recommends winehq-$WINEBRANCHNAME -y
+    apt-get install winetricks cabextract p11-kit p11-kit-modules winbind samba smbclient -y
+    ;;
+  *)
+    echo "ERROR! Aborting!"
+    exit 1
+    ;;
+esac
 
 winetricks --unattended --self-update
